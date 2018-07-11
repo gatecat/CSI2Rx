1e577ce3016dedbb9c12ef448c5975e7d9299b8f
David Shah <davey1576@gmail.com>
2018-07-11 13:11:45 +0200

    phy: Adding synchroniser to iserdes output
    Signed-off-by: David Shah <davey1576@gmail.com>

--- verilog_cores/phy/dphy_iserdes.v
+++ verilog_cores/phy/dphy_iserdes.v
@@ -33,10 +33,11 @@ module dphy_iserdes(
   input [1:0] din, // Input from arch DDR primitive, D1 should be the bit after D0
 	input sys_clk, // System byte clock
 	input areset, // Active high async reset
-	output reg [7:0] dout // Output data
+	output [7:0] dout // Output data
 );
 
 	parameter REG_INPUT = 1'b0;
+	parameter NUM_OUT_SYNCFFS = 2;
 	wire [1:0] iserdes_din;
 
 	generate
@@ -61,9 +62,17 @@ module dphy_iserdes(
 		else
 			reg_word <= {iserdes_din, reg_word[5:0]}; // MIPI interface uses LSB first
 
+	reg [7:0] out_sync_regs[0:NUM_OUT_SYNCFFS-1];
+	integer i;
 	always @(posedge sys_clk, posedge areset)
 		if (areset)
-			dout <= 0;
-		else
-			dout <= reg_word;
+			for (i = 0; i < NUM_OUT_SYNCFFS; i = i + 1)
+				out_sync_regs[i] <= 0;
+		else begin
+			for (i = 1; i < NUM_OUT_SYNCFFS; i = i + 1)
+				out_sync_regs[i] <= out_sync_regs[i-1];
+			out_sync_regs[0] <= reg_word;
+		end
+
+	assign dout = out_sync_regs[NUM_OUT_SYNCFFS-1];
 endmodule
